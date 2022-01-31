7607af4dafbcb089b1b01a013e162974de54f19d
Rodrigo Fontes <rodrifontes@gmail.com>
2022-01-31 11:47:41 -0300

    Ajuste Scroll

--- pages/chat.js
+++ pages/chat.js
@@ -168,7 +168,7 @@ function MessageList(props) {
     <Box
       tag="ul"
       styleSheet={{
-        overflow: 'scroll',
+        overflow: 'auto',
         display: 'flex',
         flexDirection: 'column-reverse',
         flex: 1,
